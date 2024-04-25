b4ebd2e9cf774a5d0f2cf832812017db21968e40
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2024-04-25 17:54:36 +0300

    Update og.sh

--- og.sh
+++ og.sh
@@ -62,11 +62,10 @@ source $HOME/.bash_profile
 fi
 
 # download binary
-cd && rm -rf 0g-evmos
-git clone https://github.com/0glabs/0g-evmos
-cd 0g-evmos
-git checkout v1.0.0-testnet
-make install
+wget https://rpc-zero-gravity-testnet.trusted-point.com/evmosd
+chmod +x ./evmosd
+mv ./evmosd /usr/local/bin/
+evmosd version
 
 # config
 evmosd config chain-id $OG_CHAIN_ID
@@ -76,15 +75,15 @@ evmosd config keyring-backend os
 evmosd init $NODENAME --chain-id $OG_CHAIN_ID
 
 # download genesis and addrbook
-wget https://github.com/0glabs/0g-evmos/releases/download/v1.0.0-testnet/genesis.json -O $HOME/.evmosd/config/genesis.json
-curl -L https://snapshots-testnet.nodejumper.io/0g-testnet/addrbook.json > $HOME/.evmosd/config/addrbook.json
+wget https://rpc-zero-gravity-testnet.trusted-point.com/genesis.json -O $HOME/.evmosd/config/genesis.json
+curl -L https://snapshot.validatorvn.com/og/addrbook.json > $HOME/.evmosd/config/addrbook.json
 
 # set minimum gas price
-sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"250000000aevmos\"|" $HOME/.evmosd/config/app.toml
+sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.00252aevmos\"|" $HOME/.evmosd/config/app.toml
 
 # set peers and seeds
-SEEDS="8c01665f88896bca44e8902a30e4278bed08033f@54.241.167.190:26656,b288e8b37f4b0dbd9a03e8ce926cd9c801aacf27@54.176.175.48:26656,8e20e8e88d504e67c7a3a58c2ea31d965aa2a890@54.193.250.204:26656"
-PEERS="1248487ea585730cdf5d3c32e0c2a43ad0cda973@peer-zero-gravity-testnet.trusted-point.com:26326"
+SEEDS="8c01665f88896bca44e8902a30e4278bed08033f@54.241.167.190:26656,b288e8b37f4b0dbd9a03e8ce926cd9c801aacf27@54.176.175.48:26656,8e20e8e88d504e67c7a3a58c2ea31d965aa2a890@54.193.250.204:26656,e50ac888b35175bfd4f999697bdeb5b7b52bfc06@54.215.187.94:26656,c9b8e7e220178817c84c7268e186b231bc943671@og-testnet-seed.itrocket.net:47656"
+PEERS="c597d920f965da0d6ca37b1a3a91be7d4586d78d@78.46.71.227:56656,312d540450524b1332cdb2af1ddffed179e47601@88.99.254.62:21656,32109a1087bcd2e8c00cd975c39353e3dd799b5f@95.217.95.10:26656,9a8ac6f12e1d1be5c999ed5184cde64473a297c3@149.102.152.54:26656,6fbb5fdd7c6ef88fa89db0cb0ffe8086ee63d511@135.181.6.189:26656,ca31cf94d5878eeb74eda79d01a28e6d85e5e50d@5.104.82.110:26656,19892d9b9e7eec08c07b48b52a59c5f666bdd6fd@135.181.75.121:26656,5e3fef852150c077adfbfebfba840a01d0b0801d@37.27.59.176:17656,325c942608727d45f9cb87fb2c4b4fdd6be7e314@95.217.47.14:26656,e444f30ce4bf9783ee4748f7d9b075611336594c@84.247.156.62:26656,664d2d4f0be9fa44403eb3942e68db17581be619@178.170.39.168:61156"
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.evmosd/config/config.toml
 
 # disable indexing
@@ -106,7 +105,7 @@ sed -i "s/snapshot-interval *=.*/snapshot-interval = 0/g" $HOME/.evmosd/config/a
 sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.evmosd/config/config.toml
 
 # create service
-sudo tee /etc/systemd/system/evmosd.service > /dev/null << EOF
+sudo tee /etc/systemd/system/ogd.service > /dev/null << EOF
 [Unit]
 Description=0G node service
 After=network-online.target
@@ -122,12 +121,12 @@ EOF
 
 # reset
 evmosd tendermint unsafe-reset-all --home $HOME/.evmosd --keep-addr-book
-curl https://snapshots-testnet.nodejumper.io/0g-testnet/0g-testnet_latest.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.evmosd
+curl https://snapshot.validatorvn.com/og/data.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.evmosd
 
 # start service
 sudo systemctl daemon-reload
-sudo systemctl enable evmosd
-sudo systemctl restart evmosd
+sudo systemctl enable ogd
+sudo systemctl restart ogd
 
 break
 ;;
